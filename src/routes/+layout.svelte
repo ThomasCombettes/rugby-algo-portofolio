3b271caf7f5aace0c561a17c63db5205700fc5c6
thomascombettes <thomascombettes@gmail.com>
2023-09-16 14:43:28 +0200

    Ajout Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import TopBar from './appbar.svelte'
 	import TabBar from './tabbar.svelte';
 </script>
 
@@ -8,21 +9,8 @@
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">rugby-algo</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/ThomasCombettes"
-					target="_blank"
-					rel="noreferrer"
-				>
-					github
-				</a>
-			</svelte:fragment>
-		</AppBar>
+		<TopBar/>
+	
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
 		<TabBar />
---
+++ src/routes/appbar.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">
+    ~
+</svelte:fragment>
+	<strong class="text-xl uppercase">rugby-algo</strong>
+	<svelte:fragment slot="trail"><a
+    id="button-t-b"
+    class="btn btn-sm variant-ghost-surface"
+    href="https://github.com/ThomasCombettes"
+    target="_blank"
+    rel="noreferrer"
+>
+    github
+</a></svelte:fragment>
+</AppBar>
+
+
