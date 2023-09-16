d7ffa476bdec4e269a5939b0dbe71337a60e6a8b
thomascombettes <thomascombettes@gmail.com>
2023-09-16 12:01:13 +0200

    Start

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import TabBar from './tabbar.svelte';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">rugby-algo</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/ThomasCombettes"
+					target="_blank"
+					rel="noreferrer"
+				>
+					github
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<TabBar />
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/tabbar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+        let valueSingle = "algo"
+
+</script>
+
+<p>
+    
+    <ListBox>
+        <ListBoxItem bind:group={valueSingle} name="medium" value="books">dfs</ListBoxItem>
+        <ListBoxItem bind:group={valueSingle} name="medium" value="books">bfs</ListBoxItem>
+        <ListBoxItem bind:group={valueSingle} name="medium" value="books">djisktra</ListBoxItem>
+    </ListBox>
+</p>
+
+
+<style>
+    
+
+</style>
